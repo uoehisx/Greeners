404bd31a789c7b2a36f7520845d25c2e6b7f4539
bhyerain <seunghye0307@naver.com>
2025-03-20 12:27:19 +0900

    [Chore] api / 추가

--- src/tsx/Login.tsx
+++ src/tsx/Login.tsx
@@ -11,7 +11,7 @@ const Login = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post("http://127.0.0.1:8000/api/login", {
+      const response = await axios.post("http://127.0.0.1:8000/api/login/", {
         username: id,
         password: password,
       });
